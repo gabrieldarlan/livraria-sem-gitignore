fbb3cd99a4141060b3227c079a7c9aee73dbd591
gabrieldarlan <gabriel.darlan88@gmail.com>
2020-03-22 20:33:44 -0300

    modulo 2 completo

---
+++ livraria/src/br/com/caelum/livraria/bean/VendasBean.java
@@ -0,0 +1,63 @@
+package br.com.caelum.livraria.bean;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ViewScoped;
+
+import org.primefaces.model.chart.BarChartModel;
+import org.primefaces.model.chart.ChartSeries;
+
+import br.com.caelum.livraria.dao.DAO;
+import br.com.caelum.livraria.modelo.Livro;
+import br.com.caelum.livraria.modelo.Venda;
+
+@ManagedBean
+@ViewScoped
+public class VendasBean {
+
+	public BarChartModel getVendasModel() {
+
+		BarChartModel model = new BarChartModel();
+		model.setAnimate(true);
+		
+		ChartSeries vendaSerie = new ChartSeries();
+		vendaSerie.setLabel("Vendas 2016");
+
+		List<Venda> vendas = getVendas(123);
+
+		for (Venda venda : vendas) {
+			vendaSerie.set(venda.getLivro().getTitulo(), venda.getQuantidade());
+		}
+
+		ChartSeries vendaSeries2015 = new ChartSeries();
+		vendaSeries2015.setLabel("Vendas 2015");
+
+		vendas = getVendas(12334);
+
+		for (Venda venda : vendas) {
+			vendaSeries2015.set(venda.getLivro().getTitulo(), venda.getQuantidade());
+		}
+
+		model.addSeries(vendaSerie);
+		model.addSeries(vendaSeries2015 );
+		return model;
+	}
+
+	public List<Venda> getVendas(long seed) {
+
+	    List<Livro> livros = new DAO<Livro>(Livro.class).listaTodos();
+	    List<Venda> vendas = new ArrayList<Venda>();
+
+	    Random random = new Random(seed);
+
+	    for (Livro livro : livros) {
+	        Integer quantidade = random.nextInt(500);
+	        vendas.add(new Venda(livro, quantidade));
+	    }
+
+	    return vendas;
+	}
+}
